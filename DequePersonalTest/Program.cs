52dbd6b50be32d430d034a048e3edbae71ba2f33
Jakub <jakub.kolsovski@gmail.com>
2019-04-08 22:52:59 +0200

    More tests

--- DequePersonalTest/Program.cs
+++ DequePersonalTest/Program.cs
@@ -11,13 +11,24 @@ namespace DequePersonalTest
 
             for (int i = 0; i < 80; i++)
             {
-                deque.PushBack(i);
-                //deque.PushFront(i);
+                //deque.PushBack(i);
+                deque.PushFront(i);
             }
             Console.WriteLine(deque[54]);
             Console.WriteLine(deque[10]);
             Console.WriteLine(deque[23]);
             Console.WriteLine(deque[22]);
+            int[] array = new int[80];
+            deque.CopyTo(array, 0);
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(deque.IndexOf(24));
+            Console.WriteLine(deque.Contains(70));
+            Console.WriteLine(deque.Contains(90));
+            deque.RemoveAt(10);
             Console.ReadLine();
         }
     }
